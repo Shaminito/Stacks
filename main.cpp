5d19b669f1019c3406a862d6357c1e9f65818bc0
Christian Vladimir Sucuzhanay Arévalo <sukuzhanay@gmail.com>
2020-01-21 17:13:50 +0100

    Ej. resuelto. Class Part. del 03 oct

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 using namespace std;
 struct vox{
-    int v;
+	float v;
+	string s;
     vox *sgt;
 };
 vox *peek = NULL;
-void push(int x){
+void push(float x, string y){
    vox *nvox =  new vox;
    nvox->v=x,nvox->sgt=peek;
+	nvox->s=y;
    peek=nvox;
 }
 void pop(){
@@ -23,18 +25,22 @@ void prt(){
      else
          while(aux!=NULL){
              cout<<aux->v<<" -> ";
+	          cout<<aux->s<<" -> ";
              aux=aux->sgt;
          }
 }
 int main(){
-    int op, x;
+    int op;
+    float x;
+    string y;
     do{
         cout<<"\n(1) Push    (2) Pop    (3) Print   (4) Salir \n ";
         cout<<"\n Elija ppcion  ?: ",cin>>op;
         switch (op){
             case 1:
-                cout<<"Ingresa el valor a insertar en el Stack : ??  ",cin>>x;
-                push(x);
+                cout<<"Ingresa el valor float a insertar en el Stack : ??  ",cin>>x;
+		          cout<<"Ingresa el valor string  a insertar en el Stack : ??  ",cin>>y;
+                push(x,y);
                 break;
             case 2:
                 cout<<"Eliminando ..... : ";
@@ -49,4 +55,3 @@ int main(){
         }
     }while(op!=4);
 }
-
